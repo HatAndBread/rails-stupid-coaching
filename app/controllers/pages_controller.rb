abd01d1f920b36a491b17b2bcc4f31d7d02a0e99
Joshua Hume <valencelectron@hotmail.com>
2021-01-05 20:23:09 +0900

    done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+    def ask
+    end
+    def answer
+        question = params[:Question].downcase
+        if question[0, 18] == 'i am going to work'
+          @answer = 'Great!'
+        elsif question.last == '?'
+            @answer = 'Silly question, get dressed and go to work!'
+        else
+            @answer = 'I don\'t care, get dressed and go to work!'
+        end
+    end
+end
